1ff7d172ad3df49624b9cf4fc2d3ad184f1a82f2
Marija Milosevic <80062620+marmilosev@users.noreply.github.com>
2022-12-06 18:52:43 +0100

    Two tests of Shoutem app

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,29 @@
-describe('Open Wdio Website', () => {
+/*describe('Open Wdio Website', () => {
     it('Proper title is displayed', () => {
         browser.url('https://webdriver.io/');
         expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
     })
-})
+})*/
+
+describe("Shoutem web", async () => {
+    /*it("Check if Shoutem Pricing page is loading", async () => {
+        await browser.url("https://shoutem.com/pricing/");
+
+        //await browser.pause(5000);
+        //nakon što otvorimo stranicu, pauziraj str
+
+        const heading = await $("h1");
+        await expect(heading).toHaveText("There are more ways to create apps with Shoutem");
+    });*/
+
+    /*it("Check if Social Network on Btn Build Now has a title Choose an app template", async () => {
+        await browser.url("https://shoutem.com/apps/social-network/");
+
+        const button = await $(".btn=Build now");
+        await button.click();
+
+        const heading = await $("h1");
+        await expect(heading).toHaveText("Choose an app template");
+
+    });*/
+});
---
+++ test/specs/popcat.js
@@ -0,0 +1,11 @@
+describe('OPop cat', () => {
+    
+    it('Proper title is displayed', () => {
+        browser.url('https://popcat.click/');
+        
+        
+        $(".app").click()
+    
+    })
+
+});

31bff1d0c7ed459983ff57852543e55bce64cb8b
Marija Milosevic <80062620+marmilosev@users.noreply.github.com>
2022-12-13 17:32:28 +0100

    Update popcat.js

--- test/specs/popcat.js
+++ test/specs/popcat.js
@@ -2,10 +2,8 @@ describe('OPop cat', () => {
     
     it('Proper title is displayed', () => {
         browser.url('https://popcat.click/');
-        
-        
+          
         $(".app").click()
-    
     })
 
 });
